9656691bf2cba47795daeae993150b49c79ea7b0
heeray <hhxxdnp@163.com>
2017-12-14 16:10:22 +0800

    practice_5test4

--- src/practice_5/worker.js
+++ src/practice_5/worker.js
@@ -1,1 +1,11 @@
+import Person from "../../src/practice_5/person.js";
+class Worker extends Person{
+    constructor(name,age){
+        super(name,age);
+    }
+    introduce(){
+        return Person.introduce()+" I am a Worker. I have a job.";
+    }
+}
+module.exports = Worker;
 
