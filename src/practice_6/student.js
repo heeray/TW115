499430fb4c5d35af036d572954c151c5b6a62292
heeray <hhxxdnp@163.com>
2017-12-14 16:19:38 +0800

    practice_6

--- src/practice_6/student.js
+++ src/practice_6/student.js
@@ -1,1 +1,12 @@
+import Person from "../../src/practice_6/person.js";
+class Student extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    introduce(){
+        return super.introduce()+" I am a Student. I am at Class "+ this.klass +".";
+    }
+}
+module.exports = Student;
 
