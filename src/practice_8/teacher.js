ce7a780c17f28fe32985ab2da308848122d1df78
heeray <hhxxdnp@163.com>
2017-12-14 16:27:54 +0800

    practice_8

--- src/practice_8/teacher.js
+++ src/practice_8/teacher.js
@@ -1,1 +1,15 @@
-
+import Person from "../../src/practice_8/person.js";
+class Teacher extends Person{
+    constructor(id,name,age,klass){
+        super(id,name,age);
+        this.klass = klass;
+    }
+    introduce(){
+        if(this.klass){
+            return super.introduce()+" I am a Teacher. I teach Class "+this.klass.number+".";
+        } else{
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+    }
+}
+module.exports = Teacher;
