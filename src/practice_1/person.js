96def9ce7a4b927c600c216cae749ca1990945fd
heeray <hhxxdnp@163.com>
2017-12-14 11:28:30 +0800

    practice_1_tests

--- src/practice_1/person.js
+++ src/practice_1/person.js
@@ -3,6 +3,8 @@ class Person{
         this.name = name;
         this.age = age;
     }
-    
+    introduce(){
+        return "My name is "+ this.name +". I am "+ this.age + " years old.";
+    }
 }
 module.exports = Person;
