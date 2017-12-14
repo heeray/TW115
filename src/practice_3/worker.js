049293747cfeab041deb300bc687b1e4df9d1163
heeray <hhxxdnp@163.com>
2017-12-14 16:06:28 +0800

    modify files

--- src/practice_3/worker.js
+++ src/practice_3/worker.js
@@ -1,4 +1,4 @@
-import Person from "../../src/practice_2/person.js";
+import Person from "../../src/practice_3/person.js";
 class Worker extends Person{
     constructor(name,age){
         super(name,age);
